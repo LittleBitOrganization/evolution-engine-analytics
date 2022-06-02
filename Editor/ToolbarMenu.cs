79d91c354a188dee60f2b7935088787694a429b8
Артем Богунов <bogunov_aa@edu.bstu.ru>
2022-06-02 16:53:35 +0300

    refactored config creation tool

--- Editor/ToolbarMenu.cs
+++ Editor/ToolbarMenu.cs
@@ -8,19 +8,36 @@ namespace InternalAssets.Scripts.LittleBit.Modules.Analytics.Editor
 {
     public static class ToolbarMenu
     {
+        private const string ConfigsFolderPath = "Assets/Resources/Configs";
+        private const string ResourcesPath = "Assets/Resources";
+        private const string ConfigFolderName = "Configs";
         private const string Extension = ".asset";
-        private const string RootFolder = "Assets/Resources";
 
         [MenuItem("Tools/Analytics/Create Config")]
         private static void CreateConfig()
         {
             var instance = ScriptableObject.CreateInstance<AnalyticsConfig>();
-
+            
+            CheckOrCreateDirectory();
+            
             AssetDatabase.CreateAsset(instance, GetFilePath());
 
             AssetDatabase.SaveAssets();
         }
 
-        private static string GetFilePath() => Path.Combine(RootFolder, Constants.AnalyticsConfigResourcesPath) + Extension;
+        private static void CheckOrCreateDirectory()
+        {
+            if (AssetDatabase.IsValidFolder(ResourcesPath) == false)
+            {
+                AssetDatabase.CreateFolder("Assets", "Resources");
+                AssetDatabase.CreateFolder(ResourcesPath, ConfigFolderName);
+                return;
+            }
+
+            if (AssetDatabase.IsValidFolder(ConfigsFolderPath) == false)
+                AssetDatabase.CreateFolder(ResourcesPath, ConfigFolderName);
+        }
+
+        private static string GetFilePath() => Path.Combine(ResourcesPath, Constants.AnalyticsConfigResourcesPath) + Extension;
     }
 }
