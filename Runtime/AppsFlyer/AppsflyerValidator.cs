e2315746c95175acca83a1639788cea1e89fed2a
TerentevVlad <ter.vlad.ter@gmail.com>
2024-12-13 12:57:32 +0300

    Добавил аппсфлаер валидатор

--- Runtime/AppsFlyer/AppsflyerValidator.cs
+++ Runtime/AppsFlyer/AppsflyerValidator.cs
@@ -1,21 +1,79 @@
 using System;
+using System.Collections;
+using System.Threading.Tasks;
+using LittleBitGames.Environment.Purchase;
 using UnityEngine;
 
 namespace AppsFlyerConnector
 {
-    public class AppsflyerValidator : MonoBehaviour, IAppsFlyerPurchaseValidation
+    public class AppsflyerValidator : MonoBehaviour, IAppsFlyerPurchaseValidation, IPurchaseValidator
     {
-        public event Action<string> OnDidApplyAnimationProperties;
-        public event Action<string> OnDidReceivePurchaseRevenueError;
+        public event Action<string> OnSuccessValidate;
+        public event Action<string> OnFailureValidate;
 
+        private TaskCompletionSource<bool> _taskCompletionSource = new TaskCompletionSource<bool>();
+
+        private float _timeoutError = 20;
+        public void Init(float timeoutError)
+        {
+            _timeoutError = timeoutError;
+        }
+
+        private Coroutine _coroutine;
+        public async Task<bool> ValidateAsync()
+        {
+            _coroutine = StartCoroutine(StartTimeoutError());
+            
+            _taskCompletionSource.SetCanceled();
+            try
+            {
+                var result = await _taskCompletionSource.Task;
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+                _taskCompletionSource = null;
+                return result;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        private IEnumerator StartTimeoutError()
+        {
+            yield return new WaitForSeconds(_timeoutError);
+            _taskCompletionSource.SetResult(false);
+        }
+
+        public void Reset()
+        {
+            _taskCompletionSource?.SetCanceled();
+            _taskCompletionSource = new TaskCompletionSource<bool>();
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+        }
+        
         public void didReceivePurchaseRevenueValidationInfo(string validationInfo)
         {
-            OnDidApplyAnimationProperties?.Invoke(validationInfo);
+            _taskCompletionSource.SetResult(true);
+            OnSuccessValidate?.Invoke(validationInfo);
         }
 
         public void didReceivePurchaseRevenueError(string error)
         {
-            OnDidReceivePurchaseRevenueError?.Invoke(error);
+            _taskCompletionSource.SetResult(false);
+            OnFailureValidate?.Invoke(error);
+        }
+
+        private void OnDestroy()
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+            }
         }
     }
 }
